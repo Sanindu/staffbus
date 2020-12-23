53f8f71070ae9371c291fe88e5da623119624a62
Sanindu <rathnayakesanindu99@gmail.com>
2020-12-24 04:07:00 +0530

    second commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+
+{ path: 'home',  component: HomeComponent },
+{ path: '', redirectTo: '/home', pathMatch: 'full'},
+{ path: 'contact',  component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
